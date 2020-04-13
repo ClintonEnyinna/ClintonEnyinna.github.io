e67106f268ebb151520cc263758936988211071b
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-04-13 09:54:32 -0500

    refactor: remove transition animation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-window.onload = function() {
-
+window.onload = function () {
   let i = 0;
   let speed = 60;
 
-  let txt = 'I strive to create solution-driven applications.';
+  let txt = "I strive to create solution-driven applications.";
   let el = document.querySelector(".content");
 
   let greet = document.querySelector(".intro p:first-of-type");
-  greet.classList.add("animate")
+  greet.classList.add("animate");
 
   setTimeout(function typeWriter() {
-
     if (i < txt.length) {
       el.innerHTML += txt.charAt(i);
       i++;
       setTimeout(typeWriter, speed);
     }
-  }, 1500)
-
-}
+  }, 1500);
+};
